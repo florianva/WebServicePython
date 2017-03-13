b5f66b2f9aedaeea5bb6d6a9999768884a569db0
Geoffrey <geoffrey.pruvost@hotmail.com>
2017-03-13 15:35:58 +0100

    allow put, delete, post request

--- WebServicePython/urls.py
+++ WebServicePython/urls.py
@@ -1,20 +1,16 @@
 from django.conf.urls import url, include
 from webservice.models import Book, Member
+from webservice.serializers import BookSerializer
+from webservice import views
 from rest_framework import routers, serializers, viewsets
 
 
 # Serializers define the API representation.
-class BookSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Book
-        fields = ('id', 'name', 'category')
-
 
 # ViewSets define the view behavior.
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
 
 
 # Serializers define the API representation.
@@ -39,6 +35,6 @@ router.register(r'members', MemberViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^books/$', views.book_list),
+    url(r'^books/(?P<pk>[0-9]+)/$', views.book_detail),
 ]
--- webservice/views.py
+++ webservice/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from webservice.models import Book
+from webservice.serializers import BookSerializer
 
-# Create your views here.
+@csrf_exempt
+def book_list(request):
+
+    if request.method == 'GET':
+        snippets = Book.objects.all()
+        serializer = BookSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = BookSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+
+@csrf_exempt
+def book_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = BookSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = BookSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
